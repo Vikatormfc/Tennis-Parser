ed113e92c12fdeac18e1680f2d5ae62a7ff754e1
Johnny McGee <johnnymcgee.dev@gmail.com>
2025-07-31 19:34:17 -0500

    Add favicon and page titles

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,63 +1,68 @@
 import DataList from "@/components/DataList";
 import {
-    formatTableData,
-    returnTableDefinition,
-    serverTableDefinition,
+  formatTableData,
+  returnTableDefinition,
+  serverTableDefinition,
 } from "@/dashboard";
 import TennisParserApi from "@/tennis-parser-api/tennis-parser-api";
 import {
-    Container,
-    Stack,
-    Tab,
-    Tabs,
-    Typography
+  Container,
+  Stack,
+  Tab,
+  Tabs,
+  Typography
 } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { redirect, useLoaderData } from "react-router";
+import { siteTitle } from "@/constants";
 
 const tables = [
-    { label: "Serving", definition: serverTableDefinition },
-    { label: "Returning", definition: returnTableDefinition },
+  { label: "Serving", definition: serverTableDefinition },
+  { label: "Returning", definition: returnTableDefinition },
 ];
 
 export async function loader({ params }) {
-    const api = new TennisParserApi();
-    if (!params.slug) {
-        const players = await api.listPlayers();
-        return redirect(`/players/${players[0].slug}`);
-    }
-    const playerDashboard = await api.getPlayerDashboard(params.slug);
+  const api = new TennisParserApi();
+  if (!params.slug) {
+    const players = await api.listPlayers();
+    return redirect(`/players/${players[0].slug}`);
+  }
+  const playerDashboard = await api.getPlayerDashboard(params.slug);
 
-    return { playerDashboard };
+  return { playerDashboard };
 }
 
 export default function DashboardPage() {
-    const { playerDashboard } = useLoaderData();
+  const { playerDashboard } = useLoaderData();
 
-    const [tab, setTab] = useState(0);
+  useEffect(() => {
+    document.title = `${playerDashboard.player} - ${siteTitle}`
+  }, [playerDashboard]);
 
-    const handleTabChange = (e, newValue) => {
-        setTab(newValue);
-    };
+  const [tab, setTab] = useState(0);
 
-    const currentTable = tables[tab] ?? tables[0];
-    const tableRows = formatTableData(currentTable.definition, playerDashboard);
+  const handleTabChange = (e, newValue) => {
+    setTab(newValue);
+  };
 
-    return (
-        <Container maxWidth="md">
-            <Stack sx={{ my: 4 }} spacing={4}>
-                <Stack spacing={2}>
-                    <Typography variant="h4" component="h1" align="center">
-                        {playerDashboard.player}
-                    </Typography>
-                    <Tabs value={tab} onChange={handleTabChange}>
-                        {tables.map(({ label }) => (
-                            <Tab key={label} label={label} />
-                        ))}
-                    </Tabs>
-                    <DataList rows={tableRows} label={currentTable.label} />
-                </Stack>
-            </Stack>
-        </Container>
-    );
+  const currentTable = tables[tab] ?? tables[0];
+  const tableRows = formatTableData(currentTable.definition, playerDashboard);
+
+  return (
+    <Container maxWidth="md">
+      <Stack sx={{ my: 4 }} spacing={4}>
+        <Stack spacing={2}>
+          <Typography variant="h4" component="h1" align="center">
+            {playerDashboard.player}
+          </Typography>
+          <Tabs value={tab} onChange={handleTabChange}>
+            {tables.map(({ label }) => (
+              <Tab key={label} label={label} />
+            ))}
+          </Tabs>
+          <DataList rows={tableRows} label={currentTable.label} />
+        </Stack>
+      </Stack>
+    </Container>
+  );
 }
